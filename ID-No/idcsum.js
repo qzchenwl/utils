bc43afa2bf6b52ec37294cd7e22a32970a717a26
Wenlong <qzchenwl@gmail.com>
2011-01-03 00:04:11 +0800

    add javascript implementation

---
+++ ID-No/idcsum.js
@@ -0,0 +1,16 @@
+function id_csum(idnum)
+{
+    var workarr = idnum.split('').reverse();
+    function W(i) {
+        return Math.pow(2, i-1)%11;
+    }
+    function S() {
+        var sum = 0;
+        for(var j=0; j<17; ++j) {
+            sum += workarr[j]*W(j+2);
+        }
+        return sum;
+    }
+    return (12-(S()%11))%11;
+}
+print(id_csum('63280119790817003')); //should return 6
